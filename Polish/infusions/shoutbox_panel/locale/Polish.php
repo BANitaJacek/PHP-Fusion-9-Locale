bf05ba50390bde9c5ef6d266c0e9eb5d389deb9e
BANitaJacek <jacek.granik@gmail.com>
2016-04-25 08:03:39 +0200

    Update Polish.php

--- Polish/infusions/shoutbox_panel/locale/Polish.php
+++ Polish/infusions/shoutbox_panel/locale/Polish.php
@@ -1,48 +1,48 @@
 <?php
 $locale['SB_title'] = "Shoutbox";
 $locale['SB_desc'] = "Shoutbox Panel";
-$locale['SB_admin1'] = "Shoutbox Admin";
+$locale['SB_admin1'] = "Shoutbox Administracja";
 
 // Shoutbox
 $locale['SB_title'] = "Shoutbox";
-$locale['SB_name'] = "Name:";
-$locale['SB_message'] = "Message:";
-$locale['SB_shout'] = "Shout";
-$locale['SB_login_req'] = "You must login to post a message.";
-$locale['SB_archive'] = "Shoutbox Archive";
-$locale['SB_no_msgs'] = "No messages have been posted.";
-$locale['SB_delete'] = "Delete";
-$locale['SB_validation_code'] = "Validation Code:";
-$locale['SB_enter_validation_code'] = "Enter Validation Code:";
-$locale['SB_edit_shout'] = "Edit Shout";
+$locale['SB_name'] = "Imię:";
+$locale['SB_message'] = "Wiadomość:";
+$locale['SB_shout'] = "Wyślij";
+$locale['SB_login_req'] = "Musisz być zalogowany aby móc pisać.";
+$locale['SB_archive'] = "Archiwum Shoutboxa";
+$locale['SB_no_msgs'] = "Wiadomość jest pusta.";
+$locale['SB_delete'] = "Usuń";
+$locale['SB_validation_code'] = "Kod Potwierdzający:";
+$locale['SB_enter_validation_code'] = "Wpisz Ważny Kod:";
+$locale['SB_edit_shout'] = "Edytuj";
 
 // Shoutbox messages
-$locale['SB_shout_updated'] = "Shout updated";
-$locale['SB_shout_deleted'] = "Shout deleted";
-$locale['SB_shouts_deleted'] = "Shouts deleted";
+$locale['SB_shout_updated'] = "Zaktualizowano";
+$locale['SB_shout_deleted'] = "Usunięto Wiadomość";
+$locale['SB_shouts_deleted'] = "Usunięto Wiadomości";
 
 // Edit Shout form
-$locale['SB_save_shout'] = "Save Shout";
+$locale['SB_save_shout'] = "Zapisz";
 
 // Prune Shoutbox
-$locale['SB_delete_old'] = "Delete Shouts older than";
-$locale['SB_days'] = " days";
-$locale['SB_delete_shouts'] = "Delete Shouts";
+$locale['SB_delete_old'] = "Usuń wiadomości po upływie";
+$locale['SB_days'] = " dni";
+$locale['SB_delete_shouts'] = "Usuń Wiadomości";
 
 // Current Shouts
-$locale['SB_edit'] = "Edit";
-$locale['SB_userip'] = "User IP: ";
-$locale['SB_on_date'] = " on ";
+$locale['SB_edit'] = "Edytuj";
+$locale['SB_userip'] = "IP Użytkownika: ";
+$locale['SB_on_date'] = " wł ";
 
 // Popup Error/Warning Messages
-$locale['SB_warning_shouts'] = "Delete shouts?";
-$locale['SB_warning_shout'] = "Delete this shout?";
+$locale['SB_warning_shouts'] = "Usunąć wiadomości?";
+$locale['SB_warning_shout'] = "Usunąć te wiadomości?";
 
 // Settings
-$locale['SB_settings'] = "Shoutbox Settings";
-$locale['SB_visible_shouts'] = "Visible Shouts";
-$locale['SB_guest_shouts'] = "Guests can shout?";
-$locale['SB_yes'] = "Yes";
-$locale['SB_no'] = "No";
-$locale['SB_submit'] = "Submit";
-$locale['SB_update_ok'] = "Settings Updated";
+$locale['SB_settings'] = "Ustawienia";
+$locale['SB_visible_shouts'] = "Widoczność Wiadmości";
+$locale['SB_guest_shouts'] = "Czy goście mają widzieć wiadomości?";
+$locale['SB_yes'] = "Tak";
+$locale['SB_no'] = "Nie";
+$locale['SB_submit'] = "Wyślij";
+$locale['SB_update_ok'] = "Zaktualizowano Ustawienia";
